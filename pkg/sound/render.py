ad4730eeca0960fc76c8c97b7f31fb3ed388c7ef
dlwlgns1214 <dlwlgns1214@naver.com>
2024-01-24 10:12:02 +0900

    첫

---
+++ pkg/sound/render.py
@@ -0,0 +1,1 @@
+from pkg.sound.echo import echo_test
---
+++ test.py
@@ -0,0 +1,11 @@
+i=0
+for i in [1,2,3,4,5,6,7]:
+    i+=1
+    print(i)
+
+def test():
+    i = 0
+    while i < 5:
+        i+=1
+        print(i)
+test()
